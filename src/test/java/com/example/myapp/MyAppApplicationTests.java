4d0c9785890556bae66d81659deb13fe1d169a52
“taojiang” <tao.jiang@thoughtworks.com>
2021-12-16 16:00:36 +0800

    modify test

--- src/test/java/com/example/myapp/MyAppApplicationTests.java
+++ src/test/java/com/example/myapp/MyAppApplicationTests.java
@@ -1,20 +1,9 @@
 package com.example.myapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 class MyAppApplicationTests {
 
-    @Container
-    public GenericContainer sql = new GenericContainer(DockerImageName.parse("postgres:last"))
-            .withExposedPorts(5432);
-
-    @Test
-    void contextLoads() {
-    }
 
 }
